277997bf7e415b56e75e429c92e7d1b13eab3132
sihadan <def6488@gmail.com>
2017-07-12 23:24:17 +0900

    made player, item script

---
+++ Papillon/Assets/Scripts/Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public int id;                  // Unique ID of item
+    public string name;             // Name of item
+    public string description;      // Item description
+    public float weight;
+
+    public delegate IEnumerator ItemEffect(Item item);
+    public ItemEffect effect;       // Item effect
+
+    private Texture2D icon;         // Item icon
+
+
+    public Item(int id, string name, string description, float weight, ItemEffect effect) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+        this.effect = effect;
+
+        LoadIcon();
+    }
+
+    private void LoadIcon() {
+        // TODO: icon load
+    }
+
+
+    public float getWeight() {
+        return this.weight;
+    }
+}
---
+++ Papillon/Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int max_health;
+    public int max_satiety;
+    public int max_armor;
+    public float max_weight;
+
+    public int health;
+    public int satiety;
+    public int armor;
+    public List<Item> items;                // Items that player holds
+    public float ItemWeightsSum;            // Sum of item weights
+    public bool ItemOverLoaded;             // ItemWeightsSum > max_weight ?
+
+    public bool inBuff;                     // player is getting buff
+
+
+    public Player(int max_health, int max_satiety, int max_armor, float max_weight) {
+        this.max_health = max_health;
+        this.max_satiety = max_satiety;
+        this.max_armor = max_armor;
+        this.max_weight = max_weight;
+
+        initializeStates();
+    }
+
+    private void initializeStates() {
+        health = max_health;
+        satiety = max_satiety;
+        armor = max_armor;
+        updateItemWeightsSum();
+    }
+
+    public void changeHealth(int d) {
+        health = Mathf.Min(max_health, health + d);
+    }
+
+    public void changeSatiety(int d) {
+        satiety = Mathf.Min(max_satiety, satiety + d);
+    }
+
+    public void changeArmor(int d) {
+        armor = Mathf.Min(max_armor, armor + d);
+    }
+
+    public bool isOverLoaded() {
+        return ItemOverLoaded == true;
+    }
+
+    public bool isOnBuff() {
+        return inBuff == true;
+    }
+
+    public bool useItem(int id) {
+        foreach(Item item in items) {
+            if(item.id == id) {
+                // TODO: try use item
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void updateItemWeightsSum() {
+        float sum = 0.0f;
+        foreach (Item item in items) {
+            sum += item.getWeight();
+        }
+
+        ItemOverLoaded = sum > max_weight;
+        ItemWeightsSum = sum;
+    }
+
+
+
+}
