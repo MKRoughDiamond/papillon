6ca489e3b634a3bd5d0335ff8636d6ca6a985f4c
sihadan <def6488@gmail.com>
2017-07-21 22:08:05 +0900

    add itemHolder class

---
+++ Papillon/Assets/Scripts/ItemHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 아이템을 드랍하는 오브젝트에 부착하는 클래스
+/// </summary>
+public class ItemHolder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
