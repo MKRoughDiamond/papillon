683488c3cc281fba8e7f3cb716d8b684db6eb6c7
sihadan <def6488@gmail.com>
2017-08-03 12:34:30 +0900

    add recipe related classes

---
+++ Papillon/Assets/Scripts/Recipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour {
+
+    public int id;                              // recipe unique id
+    public List<RecipeElement> ingredients;     // ingredients needed to generate product
+    public RecipeElement product;               // product that will be generated
+
+    public Recipe(int id, List<RecipeElement> ingredients, RecipeElement product) {
+        this.id = id;
+        this.ingredients = new List<RecipeElement>(ingredients);
+        this.product = product;
+    }
+
+    public List<RecipeElement> getIngredients() {
+        return ingredients;
+    }
+
+    public RecipeElement getProduct() {
+        return product;
+    }
+}
+
+public class RecipeElement {
+
+    public int id;
+    public int count;
+
+    public RecipeElement(int id, int count) {
+        this.id = id;
+        this.count = count;
+    }
+}
---
+++ Papillon/Assets/Scripts/RecipeDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeDatabase : MonoBehaviour {
+
+    public static Recipe findRecipe(int id) {
+        //TODO: implement
+        //return found_recipe;
+        return new Recipe(0, new List<RecipeElement>(), new RecipeElement(1, 1));
+    }
+}
