12fe3892b24cfdf9285c6e9241f9d5684b42b278
sihadan <def6488@gmail.com>
2017-08-04 18:22:35 +0900

    update map

---
+++ Papillon/Assets/Scripts/Button/UserMoveButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMoveButton : MonoBehaviour {
+
+    private Vector2 position;
+
+    public void updatePosition(int x, int y) {
+        this.position = new Vector2(x, y);
+    }
+
+    public void onClick() {
+        Map map = gameObject.GetComponentInParent<Map>();
+        map.movePlayer(position);
+        map.displayMap();
+    }
+}
