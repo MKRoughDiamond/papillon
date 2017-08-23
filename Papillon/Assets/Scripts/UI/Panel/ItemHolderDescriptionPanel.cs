7a95d3e7d5e072cf0ed88e7b1ffb4a96969562a3
sihadan <def6488@gmail.com>
2017-08-23 15:04:20 +0900

    add itemholderdescriptionpanel

---
+++ Papillon/Assets/Scripts/UI/Panel/ItemHolderDescriptionPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHolderDescriptionPanel : MonoBehaviour {
+
+    public Text name;
+    public Text description;
+
+    private ItemHolder holder;
+
+    void Start() {
+        holder = GetComponentInParent<ItemHolder>();
+        name.text = holder.possesion.getItem().getName();
+        description.text = holder.possesion.getItem().getDescription();
+    }
+}
