3060836b058b205b211cbf7dcd3d773bbb6f0dd8
MK_RD <dnjstjr1030@naver.com>
2017-08-24 22:53:51 +0900

    minor fix

--- Papillon/Assets/Scripts/UI/Text/InventoryText.cs
+++ Papillon/Assets/Scripts/UI/Text/InventoryText.cs
@@ -17,13 +17,20 @@ public class InventoryText : MonoBehaviour {
     }
 
     private void Update() {
-
-        if(player.getItemWeight() > player.getMaxItemWeight()*0.9) {
+        float currWeight = player.getItemWeight();
+        if(currWeight > player.getMaxItemWeight()*0.9) {
             inventoryText.color = Color.red;
         } else {
             inventoryText.color = originalColor;
         }
 
-        inventoryText.text = player.getItemWeight().ToString() + "/" + player.getMaxItemWeight();
+        if ((int)currWeight == currWeight)
+            inventoryText.text = currWeight.ToString() + "/" + player.getMaxItemWeight();
+        else {
+            if (currWeight < 1)
+                inventoryText.text = "0" + currWeight.ToString("#.00") + "/" + player.getMaxItemWeight();
+            else
+                inventoryText.text = currWeight.ToString("#.00") + "/" + player.getMaxItemWeight();
+        }
     }
 }
