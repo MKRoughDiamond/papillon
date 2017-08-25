9de5b3d0a9308de1cf3201bd150f41537ac0b208
sihadan <def6488@gmail.com>
2017-08-25 12:22:10 +0900

    hos

---
+++ Papillon/Assets/Scripts/UI/BlackholeRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeRotator : MonoBehaviour {
+
+    RectTransform r;
+
+	void Start () {
+        r = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        r.Rotate(new Vector3(0, 0, -1));
+	}
+}
